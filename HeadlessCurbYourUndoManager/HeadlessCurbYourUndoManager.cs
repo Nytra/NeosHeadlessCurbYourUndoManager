0499162e496a0a61d6f6e1a8c5165972170018e9
Sam Scott <samueltscott@gmail.com>
2023-06-02 01:25:59 +0100

    Disable the mod after it runs the destroy code and also when user spawns in the world

--- HeadlessCurbYourUndoManager/HeadlessCurbYourUndoManager.cs
+++ HeadlessCurbYourUndoManager/HeadlessCurbYourUndoManager.cs
@@ -3,63 +3,87 @@ using FrooxEngine.Undo;
 using HarmonyLib;
 using NeosModLoader;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace HeadlessCurbYourUndoManager
 {
-    public class HeadlessCurbYourUndoManager : NeosMod
-    {
-        public override string Name => "HeadlessCurbYourUndoManager";
-        public override string Author => "Nytra";
-        public override string Version => "1.0.2";
-        public override string Link => "https://github.com/Nytra/NeosHeadlessCurbYourUndoManager";
+	public class HeadlessCurbYourUndoManager : NeosMod
+	{
+		public override string Name => "HeadlessCurbYourUndoManager";
+		public override string Author => "Nytra";
+		public override string Version => "1.0.3";
+		public override string Link => "https://github.com/Nytra/NeosHeadlessCurbYourUndoManager";
 
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony($"owo.{Author}.{Name}");
-            Type neosHeadless = AccessTools.TypeByName("NeosHeadless.Program");
-            if (neosHeadless != null)
-            {
-                Debug("Patching.");
-                harmony.PatchAll();
-            }
-        }
+		private static Dictionary<World, bool> worldDontDestroyMap = new Dictionary<World, bool>();
 
-        [HarmonyPatch(typeof(UndoManager), "OnAttach")]
-        class HeadlessCurbYourUndoManagerPatch
-        {
-            public static bool Prefix(UndoManager __instance)
-            {
-                Debug($"UndoManager attached in world {__instance.World.Name}!");
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony($"owo.{Author}.{Name}");
+			Type neosHeadless = AccessTools.TypeByName("NeosHeadless.Program");
+			if (neosHeadless != null)
+			{
+				Debug("Patching.");
+				harmony.PatchAll();
+			}
+		}
 
-                __instance.World.RootSlot.ChildAdded += (slot, child) =>
-                {
-                    child.RunInUpdates(0, () =>
-                    {
-                        if (child.Name == "Undo Manager")
-                        {
-                            Debug("Found the extra Undo Manager slot!");
-                            var undoManager = child.GetComponent<UndoManager>();
-                            if (undoManager != null && child.World.RootSlot.GetComponentsInChildren<UndoManager>().Count >= 2)
-                            {
-                                Debug("Found the extra UndoManager component! Destroying it!");
+		[HarmonyPatch(typeof(UndoManager), "OnAttach")]
+		class HeadlessCurbYourUndoManagerPatch
+		{
+			public static bool Prefix(UndoManager __instance)
+			{
+				Debug($"UndoManager attached in world {__instance.World.Name}!");
 
-                                // The slot is marked as protected, so it cannot be destroyed, so here I am just renaming it to something else
-                                child.Name = "Destroyed Undo Manager";
-                                undoManager.Destroy();
+				if (!worldDontDestroyMap.ContainsKey(__instance.World))
+				{
+					worldDontDestroyMap.Add(__instance.World, false);
+					__instance.World.RootSlot.ChildAdded += (slot, child) =>
+					{
+						if (worldDontDestroyMap[child.World] == false)
+						{
+							child.RunInUpdates(0, () =>
+							{
+								if (child.Name == "Undo Manager")
+								{
+									Debug("Found the extra Undo Manager slot!");
+									var undoManager = child.GetComponent<UndoManager>();
+									if (undoManager != null && child.World.RootSlot.GetComponentsInChildren<UndoManager>().Count >= 2)
+									{
+										Debug("Found the extra UndoManager component! Destroying it!");
 
-                                Debug("Respawning all users.");
-                                foreach (User u in __instance.World.FindUsers((User u2) => u2.ReferenceID != __instance.LocalUser.ReferenceID))
-                                {
-                                    u.Root.Slot.Destroy();
-                                }
-                            }
-                        }
-                    });
-                };
+										// The slot is marked as protected, so it cannot be destroyed, so here I am just renaming it to something else
+										child.Name = "Destroyed Undo Manager";
+										undoManager.Destroy();
 
-                return true;
-            }
-        }
-    }
+										Debug("Respawning all users.");
+										foreach (User u in child.World.FindUsers((User u2) => u2.ReferenceID != child.LocalUser.ReferenceID))
+										{
+											u.Root.Slot.Destroy();
+										}
+
+										worldDontDestroyMap[child.World] = true;
+
+										Debug($"Mod has been disabled in world {child.World.Name}.");
+									}
+								}
+							});
+						}
+					};
+
+					__instance.World.UserSpawn += (user) =>
+					{
+						if (worldDontDestroyMap[user.World] == false)
+						{
+							// user spawn disables the mod
+							worldDontDestroyMap[user.World] = true;
+							Debug($"User spawned. Mod has been disabled in world {user.World.Name}.");
+						}
+					};
+				}
+
+				return true;
+			}
+		}
+	}
 }
