7f960604539b46b4aaacfbac5f5a6a59bb72791e
Sam Scott <samueltscott@gmail.com>
2023-06-03 05:56:38 +0100

    Fix AssemblyInfo

--- HeadlessCurbYourUndoManager/Properties/AssemblyInfo.cs
+++ HeadlessCurbYourUndoManager/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HeadlessCurbYourUndoManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nytra")]
 [assembly: AssemblyProduct("HeadlessCurbYourUndoManager")]
 [assembly: AssemblyCopyright("Copyright © Nytra 2023")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
